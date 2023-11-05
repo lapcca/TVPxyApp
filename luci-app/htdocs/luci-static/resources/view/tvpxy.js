5df2d48003355f975feafc2e18a939cc6b4c84d7
Jan <lapcca@163.com>
2023-11-06 00:12:19 +0800

    firt init

---
+++ luci-app/htdocs/luci-static/resources/view/tvpxy.js
@@ -0,0 +1,30 @@
+'use strict';
+'require form';
+'require view';
+
+return view.extend({
+	render: function () {
+		var m, s, o;
+
+		m = new form.Map('tvpxy', _('tvpxy'),
+			_('TVPxy is a light-weight proxy server. It can retrieve the RTP streams and forward it to internal LAN over HTTP.'));
+
+		s = m.section(form.TypedSection, 'tvpxy');
+		s.anonymous = true;
+		s.addremove = true;
+
+		o = s.option(form.Flag, 'disabled', _('Enabled'));
+		o.enabled = '0';
+		o.disabled = '1';
+		o.default = o.disabled;
+		o.rmempty = false;
+
+		o = s.option(form.Value, 'port', _('Listen Port'));
+		o.datatype = 'port';
+
+		o = s.option(form.Value, 'netdev', _('RTP Source IP/Interface'));
+		o.datatype = 'or(ipaddr, network)';
+
+		return m.render();
+	}
+});
